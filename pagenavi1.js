d3957969cc3e4c11a38bce662b52c39cad3a518d
chaucuto100 <40693776+chaucuto100@users.noreply.github.com>
2018-07-02 10:51:30 +0700

    Add files via upload

---
+++ pagenavi1.js
@@ -0,0 +1,10 @@
+  /*<![CDATA[*/
+    var perPage=48;
+    var numPages=4;
+    var firstText ='';
+    var lastText ='';
+    var prevText ='';
+    var nextText ='';
+    var urlactivepage=location.href;
+    var home_page="/";
+  /*]]>*/
