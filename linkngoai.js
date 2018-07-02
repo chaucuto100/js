b6c173eca5ff651441d4e1df4622cbef9eb375e3
chaucuto100 <40693776+chaucuto100@users.noreply.github.com>
2018-07-02 10:49:39 +0700

    Add files via upload

---
+++ linkngoai.js
@@ -0,0 +1,14 @@
+//<![CDATA[       
+function changetargetlink(x){       
+var a = document.getElementsByTagName('a');       
+var args = changetargetlink.arguments;       
+for(var i in a){       
+a[i].target = "_blank";       
+a[i].rel = "_blank";       
+for(var j=0;j<args.length;j++){       
+if(a[i].href.match(args[j])){       
+a[i].target = "";       
+a[i].rel = "";       
+}}}}       
+  changetargetlink('nguyenminhchau1993.blogspot.com','https://nguyenminhchau1993.blogspot.com');       
+//]]>  
